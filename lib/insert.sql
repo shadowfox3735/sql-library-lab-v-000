057d2ee09fa66dad32dd69ed3ce32f05bdda31cb
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 22:46:24 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,45 @@
+INSERT INTO series (title, author_id, subgenre_id)
+VALUES (null, null, null),
+(null, null, null);
+
+INSERT INTO subgenres (name)
+VALUES (null), (null);
+
+INSERT INTO authors (name)
+VALUES (null), (null);
+
+INSERT INTO books (title, year, series_id)
+VALUES (null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null),
+(null, null, null);
+
+INSERT INTO characters (name, motto, species, author_id, series_id)
+VALUES (null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null),
+(null, null, null, null, null);
+
+INSERT INTO character_books (book_id, character_id)
+VALUES (null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null),
+(null, null);
