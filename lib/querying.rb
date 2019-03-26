91503185f15210b90dda68dbfef9c284210aa938
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 22:51:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,6 +16,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors 
   JOIN series ON authors.id = series.author_id 
   JOIN subgenres ON subgenres.id = series.subgenre_id"
+end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
